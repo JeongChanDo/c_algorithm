3b6e5f792de3cff8d69504b34cc9ac6a89f759ec
JeongChanDo <djc4223@naver.com>
2022-05-04 18:32:02 +0900

    add size of int array and binary search

--- c_algorithm/ex01_array.c
+++ c_algorithm/ex01_array.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define SizeIntArray(arr) sizeof(arr)/sizeof(int)
+
 
 int SumArray(int arr[], int size)
 {
@@ -15,12 +17,34 @@ int SequentialSearch(int arr[], int size, int value)
 	return -1;
 }
 
+
+int BinarySearch(int arr[], int size, int value)
+{
+	int low = 0, mid;
+	int high = size - 1;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		if (arr[mid] == value) return mid;
+		else if (arr[mid] < value) low = mid + 1;
+		else high = mid - 1;
+	}
+	return -1;
+}
+
 int main()
 {
 	int arr1[] = {1, 2, 3, 4, 5};
 	printf("ex1 배열 합 : %d \n", SumArray(arr1, 5));
 	int arr2[] = { 43, 23, 64, 12 };
 	printf("ex2 순차 탐색 (찾는 값의 인덱스) : %d \n", SequentialSearch(arr2, 4, 64));
+	int arr3[] = { 12, 34, 35, 36, 46, 54, 66, 76, 130, 254, 256, 267, 414, 756, 1530, 2467};
+	printf("* 배열의 크기 : %d \n", sizeof(arr3)/sizeof(int));
+	printf("* 배열의 크기 2: %d \n", SizeIntArray(arr3));
+
+	printf("ex3 이진 검색 : %d \n", BinarySearch(arr3, SizeIntArray(arr3), 54));
+	printf("ex3 이진 검색 : %d \n", BinarySearch(arr3, SizeIntArray(arr3), 66));
+
 
 
 }
