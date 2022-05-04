18211390515fc82c48dd46b3c4dd480b5601e8af
JeongChanDo <djc4223@naver.com>
2022-05-04 18:18:28 +0900

    add Sequential Search

--- c_algorithm/ex01_array.c
+++ c_algorithm/ex01_array.c
@@ -8,8 +8,19 @@ int SumArray(int arr[], int size)
 	return total;
 }
 
+int SequentialSearch(int arr[], int size, int value)
+{
+	int i = 0;
+	for (i = 0; i < size; i++) if (value == arr[i]) return i;
+	return -1;
+}
+
 int main()
 {
 	int arr1[] = {1, 2, 3, 4, 5};
 	printf("ex1 배열 합 : %d \n", SumArray(arr1, 5));
+	int arr2[] = { 43, 23, 64, 12 };
+	printf("ex2 순차 탐색 (찾는 값의 인덱스) : %d \n", SequentialSearch(arr2, 4, 64));
+
+
 }
